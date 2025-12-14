bcd2fe988d781ab0115bd585ee1347d3f7f3a5b7
Devz-0047 <devendersingh00747@gmail.com>
2025-12-14 15:02:17 +0530

    changed button

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/concept/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            FireScore Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
